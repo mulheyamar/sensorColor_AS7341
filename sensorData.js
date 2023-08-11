1764807ee27eb0152e56616569f327642fab049f
mulheyamar <muheyamar@gmail.com>
2023-08-11 04:49:25 +0200

    Calculo de Basic Counts

---
+++ sensorData.js
@@ -0,0 +1,136 @@
+//crear variable para almacenar los datos obtenidos por el sensor de las lecturas de los colores "target"
+const datosSensor = [{
+        color: 'violeta',
+        F1: 489,
+        F2: 4810,
+        F3: 1250,
+        F4: 1374,
+        F5: 948,
+        F6: 995,
+        F7: 746,
+        F8: 419,
+        clear: 5360,
+        IR: 310
+    },
+    {
+        color: 'azul',
+        F1: 497,
+        F2: 5096,
+        F3: 1395,
+        F4: 1696,
+        F5: 1221,
+        F6: 570,
+        F7: 362,
+        F8: 291,
+        clear: 5466,
+        IR: 292
+    },
+    {
+        color: 'cyan',
+        F1: 605,
+        F2: 5634,
+        F3: 1984,
+        F4: 3558,
+        F5: 3963,
+        F6: 1818,
+        F7: 625,
+        F8: 451,
+        clear: 8676,
+        IR: 476
+    },
+    {
+        color: 'verde',
+        F1: 209,
+        F2: 615,
+        F3: 870,
+        F4: 3204,
+        F5: 5412,
+        F6: 2775,
+        F7: 686,
+        F8: 344,
+        clear: 6140,
+        IR: 518
+    },
+    {
+        color: 'verde_lima',
+        F1: 255,
+        F2: 708,
+        F3: 920,
+        F4: 3263,
+        F5: 5691,
+        F6: 3918,
+        F7: 1502,
+        F8: 652,
+        clear: 7268,
+        IR: 602
+    },
+    {
+        color: 'amarillo',
+        F1: 272,
+        F2: 748,
+        F3: 796,
+        F4: 2342,
+        F5: 4268,
+        F6: 5208,
+        F7: 3099,
+        F8: 1259,
+        clear: 7613,
+        IR: 692
+    },
+    {
+        color: 'naranja',
+        F1: 176,
+        F2: 558,
+        F3: 350,
+        F4: 619,
+        F5: 1511,
+        F6: 3933,
+        F7: 2856,
+        F8: 1127,
+        clear: 4656,
+        IR: 518
+    },
+    {
+        color: 'rojo',
+        F1: 158,
+        F2: 518,
+        F3: 262,
+        F4: 304,
+        F5: 1014,
+        F6: 3690,
+        F7: 2807,
+        F8: 1099,
+        clear: 4091,
+        IR: 492
+    },
+    {
+        color: 'blanco',
+        F1: 799,
+        F2: 5906,
+        F3: 2382,
+        F4: 4834,
+        F5: 6912,
+        F6: 6399,
+        F7: 3533,
+        F8: 1600,
+        clear: 14416,
+        IR: 940
+    },
+    {
+        color: 'negro',
+        F1: 30,
+        F2: 288,
+        F3: 107,
+        F4: 158,
+        F5: 220,
+        F6: 184,
+        F7: 115,
+        F8: 55,
+        clear: 647,
+        IR: 170
+    },
+
+];
+
+// exporto la variable
+module.exports = datosSensor;
